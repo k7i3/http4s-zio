350de4290c1207151479590ce5c4b02bdef77a53
yakovlevkv <yakovlevkv@vtblife.ru>
2020-01-10 17:27:38 +0300

    =>

--- src/main/scala/ru/vtblife/classified/ska/Main.scala
+++ src/main/scala/ru/vtblife/classified/ska/Main.scala
@@ -1,11 +1,11 @@
 package ru.vtblife.classified.ska
 
 import cats.effect.ExitCode
-import zio.{Task, URIO}
+import zio.{Task, URIO, ZEnv}
 import zio.interop.catz._
 import zio.interop.catz.implicits._
 
 object Main extends CatsApp {
-  def run(args: List[String]): URIO[Any, Int] =
+  override def run(args: List[String]): URIO[ZEnv, Int] =
     SkaServer.stream[Task].compile[Task, Task, ExitCode].drain.fold(_ => 1, _ => 0)
 }
